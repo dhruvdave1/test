9bac0aa3b8ab6a61464d592549e29162ac416103
dhruvdave1 <91867129+dhruvdave1@users.noreply.github.com>
2025-05-27 19:38:38 +0530

    Update test.tsx

--- test.tsx
+++ test.tsx
@@ -1,5 +1,3 @@
-            <AtomButton
-              variant="text"
-              size="small"
-              className="text-button regenerate-text"
-              onClick={onClickRegenerate}
+qadsfghfgdsasdhgj,.lfgdsa
+
+                        
